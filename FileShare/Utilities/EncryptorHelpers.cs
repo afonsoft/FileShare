dbc49ce476a65c14dddca03ff1aedaad7396429e
Afonso Dutra Nogueira Filho <adfilho@voegol.com.br>
2023-03-22 16:59:56 -0300

    EncryptorHelpers

--- FileShare/Utilities/EncryptorHelpers.cs
+++ FileShare/Utilities/EncryptorHelpers.cs
@@ -10,7 +10,13 @@ namespace FileShare.Utilities
             using (var md5 = MD5.Create())
             {
                 var result = md5.ComputeHash(Encoding.ASCII.GetBytes(text));
-                return Encoding.ASCII.GetString(result).Trim().ToUpper();
+                // Convert the byte array to hexadecimal string prior to .NET 5
+                StringBuilder sb = new System.Text.StringBuilder();
+                for (int i = 0; i < result.Length; i++)
+                {
+                    sb.Append(result[i].ToString("X2"));
+                }
+                return sb.ToString();
             }
         }
     }
